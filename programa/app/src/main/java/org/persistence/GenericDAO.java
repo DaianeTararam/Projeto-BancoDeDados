9d28c0da56da5481faebc1feba0eda3d8cd84c2e
João Francisco Alves da Silva <132608211+Joaoftito@users.noreply.github.com>
2025-06-19 03:29:01 -0300

    programa funcional

---
+++ programa/app/src/main/java/org/persistence/GenericDAO.java
@@ -0,0 +1,26 @@
+package org.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GenericDAO {
+	
+	private Connection connection;
+	
+	public Connection getConnection(String dbName) throws SQLException, ClassNotFoundException {
+		
+		String hostName = "localhost";
+		String userName = "sa";
+		String password = "123456";
+		
+		Class.forName("net.sourceforge.jtds.jdbc.Driver");
+		connection = DriverManager.getConnection(
+				String.format("jdbc:jtds:sqlserver://%s:1433;databaseName=%s;user=%s;password=%s",
+						hostName, dbName, userName, password)
+				);
+		return connection;
+	}
+	
+
+}
