80c90431ed3ace7dcdb080b2d066f9e8bd5fa3f3
João Francisco Alves da Silva <132608211+Joaoftito@users.noreply.github.com>
2025-06-20 23:49:49 -0300

    Add files via upload

--- programa/app/src/main/java/org/model/Comanda.java
+++ programa/app/src/main/java/org/model/Comanda.java
@@ -2,14 +2,11 @@ package org.model;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.time.LocalDate;
 
 public class Comanda {
 	
 	private int codigo;
 	private List<Item> itens = new ArrayList<>();
-	private LocalDate dataHora = LocalDate.now();
-	private double valorTotal;
 	
 	public int getCodigo() {
 		return codigo;
@@ -26,22 +23,6 @@ public class Comanda {
 	public void setItens(List<Item> itens) {
 		this.itens = itens;
 	}
-	
-	public LocalDate getDataHora() {
-		return dataHora;
-	}
-	
-	public void setDataHora(LocalDate dataHora) {
-		this.dataHora = dataHora;
-	}
-	
-	public double getValorTotal() {
-		return valorTotal;
-	}
-	
-	public void setValorTotal(double valorTotal) {
-		this.valorTotal = valorTotal;
-	}	
 	
 	
 }
