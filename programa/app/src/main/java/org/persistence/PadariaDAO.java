e226a3a69962e647da2d241d06c090771644c3e6
JM <oshirohiro00@gmail.com>
2025-06-21 21:02:52 -0300

    Update PadariaDAO.java

--- programa/app/src/main/java/org/persistence/PadariaDAO.java
+++ programa/app/src/main/java/org/persistence/PadariaDAO.java
@@ -1,40 +1,13 @@
 package org.persistence;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.model.Item;
 import org.model.Produto;
 
-public class PadariaDAO {
-	
-	private GenericDAO gDao;
-	private Connection c;
-	
-	public PadariaDAO(GenericDAO gDao) {
-		this.gDao = gDao;
-	}
-	
-	public List<Produto> getProdutos() throws ClassNotFoundException, SQLException {
-		String sql = "SELECT codigo, nome, valorUnitario FROM Produto";
-		c = gDao.getConnection("padariaBD");
-		PreparedStatement ps = c.prepareStatement(sql);
-		ResultSet rs = ps.executeQuery();
-		List<Produto> produtos = new ArrayList<>();
-		while (rs.next()) {
-			Produto produto = new Produto();
-			produto.setCodigo(rs.getInt("codigo"));
-			produto.setNome(rs.getString("nome"));
-			produto.setValorUnitario(rs.getDouble("valorUnitario"));
-			
-			produtos.add(produto);
-		}
-		rs.close();
-		ps.close();
-		return produtos;
-	}
-
+public interface PadariaDAO {
+    public List<Produto> getProdutos() throws SQLException;
+    public List<Item> lerTodosItens(int codigo) throws SQLException;
+    //colocar o restante aqui
 }
