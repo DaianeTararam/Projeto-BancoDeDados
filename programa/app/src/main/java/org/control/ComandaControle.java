8fb7eafc7b4f7e77dae49b8fb6ced03dcee9f43e
JM <oshirohiro00@gmail.com>
2025-06-21 21:05:10 -0300

    Update ComandaControle.java

--- programa/app/src/main/java/org/control/ComandaControle.java
+++ programa/app/src/main/java/org/control/ComandaControle.java
@@ -1,26 +1,38 @@
 package org.control;
 
-import org.model.Comanda;
-import org.model.Item;
 import org.persistence.PadariaDAO;
+import org.persistence.PadariaDAOImplementation;
+
+import java.util.List;
+
+import org.model.*;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class ComandaControle {
     private ObservableList<Item> lista = FXCollections.observableArrayList();
 
-    //private PadariaDAO padariaDAO = new PadariaDAO();
+    private PadariaDAO padariaDAO = new PadariaDAOImplementation();
     private StringProperty codigoProduto = new SimpleStringProperty("");
     private StringProperty nomeProduto = new SimpleStringProperty("");
     private StringProperty quantidade = new SimpleStringProperty("");
     private Comanda comandaAtual;
 
     // public Comanda buscarCriarComanda(int codigo){
-        
     // }
+    
+    //metodo retorna() é apenas um teste
+    public void retorna() throws SQLException{
+        List<Produto> lista = new ArrayList<>();
+        lista = padariaDAO.getProdutos();
+        lista.forEach(al -> System.out.println(al.toString()));
+    }
 
     public void addItem(int codigo){
         //
