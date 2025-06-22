6986c4cc3e39c8fdc48d02de49f3e56c8e2c676e
JM <oshirohiro00@gmail.com>
2025-06-22 13:23:17 -0300

    Update ComandaControle.java

--- programa/app/src/main/java/org/control/ComandaControle.java
+++ programa/app/src/main/java/org/control/ComandaControle.java
@@ -16,7 +16,6 @@ import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ComandaControle {
@@ -26,34 +25,40 @@ public class ComandaControle {
     private LongProperty codigoProduto = new SimpleLongProperty(0);
     private StringProperty nomeProduto = new SimpleStringProperty("");
     private IntegerProperty quantidade = new SimpleIntegerProperty(0);
-    private Comanda comandaAtual;
+    private Comanda comandaAtual = null;
 
-    public Comanda buscarComanda(long codigo) throws SQLException{
-        comandaAtual = padariaDAO.getComanda(codigo);
+    public boolean buscarComanda(long codigo){
+        Comanda teste = padariaDAO.getComanda(codigo);
+        if (teste != null){
+            this.comandaAtual = teste;
+            return true;
+        }
         
-        return comandaAtual;
+        return false;
     }
 
+    //metodo para validar comanda atual
+
     //apenas um teste pro botao salvar
-    public void retorna() throws SQLException{
+    public void retorna(){
         List<Produto> lista = new ArrayList<>();
         lista = padariaDAO.getProdutos();
         lista.forEach(al -> System.out.println(al.toString()));
     }
 
     //tem q terminar
-    public void addItem() throws SQLException{
+    public void addItem(){
         Produto produto = new Produto();
         produto = padariaDAO.getProduto(codigoProduto.get());
         if (produto != null){
             Item item = telaParaItens(produto);
             //padariaDAO.guardar(item);
-            lista.add(item); 
-            //pesquisarItem();
+            lista.add(item); //??
+            //pesquisarItem(); <- ver pra q q serve
         }
     }
 
-    public void atualizaTabela() throws SQLException{
+    public void atualizaTabela(){
         lista.clear();
         lista.addAll(padariaDAO.getItens(comandaAtual.getCodigo()));
     }
