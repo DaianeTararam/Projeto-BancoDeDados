d555a04a1859df484f4df1ad54f599578ad4df5f
JM <oshirohiro00@gmail.com>
2025-06-23 00:38:38 -0300

    Update PadariaDAO.java

--- programa/app/src/main/java/org/persistence/PadariaDAO.java
+++ programa/app/src/main/java/org/persistence/PadariaDAO.java
@@ -7,10 +7,12 @@ import org.model.Item;
 import org.model.Produto;
 
 public interface PadariaDAO {
-    public List<Produto> getProdutos();
     public List<Item> lerTodosItens(int codigo);
     public Produto getProduto(long codigo);
     List<Item> getItens(long comanda) ;
     Comanda getComanda(long codigo);
+    void guardar(Item item, long codigoComanda);
+    boolean atualizar(Item item, long codigoComanda);
+    boolean excluir(long codigoComanda, long codigoProduto);
     //colocar o restante aqui
 }
