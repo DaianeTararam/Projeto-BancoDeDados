2cdbd8ac91a87aca8a69caca655c5d0d575e532b
JM <oshirohiro00@gmail.com>
2025-06-23 22:09:53 -0300

    Update PadariaDAO.java

--- programa/app/src/main/java/org/persistence/PadariaDAO.java
+++ programa/app/src/main/java/org/persistence/PadariaDAO.java
@@ -1,13 +1,19 @@
 package org.persistence;
 
-import org.model.ProdutoPedido;
-import org.model.Pedido;
+import java.util.List;
+
+import org.model.Comanda;
+import org.model.Item;
+import org.model.Produto;
 
 public interface PadariaDAO {
-    void novoProdutoPedido(ProdutoPedido produtoPedido);
-    void novoPedido(Pedido pedido);
-    void consultaTodosPedidos();
-    void consultaTotalVendasPedidos();
-    void consultaQtdTotalProdutosVendidosCategoria();
-    void consultaPedidosValorTotalAcima();
+    public List<Item> lerTodosItens(int codigo);
+    public Produto getProduto(long codigo);
+    List<Item> getItens(long comanda) ;
+    Comanda getComanda(long codigo);
+    void guardar(Item item, long codigoComanda);
+    void excluir(long codigoComanda, long codigoProduto);
+    boolean verificaProdutoLista(long codigoComanda, long codigoProduto);
+    void editarQuantidadeProduto(int quantidade, long codigoComanda, long codigoProduto);
+    //colocar o restante aqui
 }
