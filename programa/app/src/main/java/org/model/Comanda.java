2c1a337eff50c1b7392c054c2efeb9bd8ef2c342
JM <oshirohiro00@gmail.com>
2025-06-23 22:03:10 -0300

    Update Comanda.java

--- programa/app/src/main/java/org/model/Comanda.java
+++ programa/app/src/main/java/org/model/Comanda.java
@@ -1,8 +1,12 @@
 package org.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Comanda {
-	
 	private long codigo;
+	private List<Item> itens = new ArrayList<>();
 	
 	public long getCodigo() {
 		return codigo;
@@ -10,6 +14,14 @@ public class Comanda {
 	
 	public void setCodigo(long codigo) {
 		this.codigo = codigo;
+	}
+	
+	public List<Item> getItens() {
+		return itens;
+	}
+	
+	public void setItens(List<Item> itens) {
+		this.itens = itens;
 	}
 	
 	
