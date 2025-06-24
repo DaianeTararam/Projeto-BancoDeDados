f49012748e491a3e2fddcd4b46977dec92fd5d9b
JM <oshirohiro00@gmail.com>
2025-06-23 22:01:57 -0300

    Update AtendenteFronteira.java

--- programa/app/src/main/java/org/view/AtendenteFronteira.java
+++ programa/app/src/main/java/org/view/AtendenteFronteira.java
@@ -2,12 +2,15 @@ package org.view;
 
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.BorderPane;
@@ -16,33 +19,93 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import javafx.util.converter.IntegerStringConverter;
 import javafx.util.converter.LongStringConverter;
+import javafx.scene.control.TableView;
 import javafx.geometry.Insets;
 
 import java.sql.SQLException;
 
 import org.control.ComandaControle;
+import org.model.Item;
+
 
 public class AtendenteFronteira extends Application{
     private ComandaControle controle = new ComandaControle();
     private TextField txtCodigoProduto = new TextField();
     private TextField txtQuantidade = new TextField();
     private TextField txtCodigoComanda = new TextField();
-    private TextField txtCodigoFormaPagamento = new TextField();
+
+    private TableView<Item> tabela = new TableView<>();
 
     public void bindings(){
         Bindings.bindBidirectional(txtCodigoProduto.textProperty(), controle.codigoProdutoProperty().asObject(), new LongStringConverter());
         Bindings.bindBidirectional(txtQuantidade.textProperty(), controle.quantidadeProperty().asObject(), new IntegerStringConverter());
     }
 
+    public void tableCreation(){
+        TableColumn<Item, String> col1 = new TableColumn<>("Codigo");
+        col1.setCellValueFactory(c -> {
+            String strCodigo = Long.toString(c.getValue().getProduto().getCodigo());
+            return new ReadOnlyStringWrapper(strCodigo);
+        });
+
+        TableColumn<Item, String> col2 = new TableColumn<>("Produto");
+        col2.setCellValueFactory(c -> new ReadOnlyStringWrapper(c.getValue().getProduto().getNome()));
+
+        TableColumn<Item, String> col3 = new TableColumn<>("Quantidade");
+        col3.setCellValueFactory(c -> {
+            String strQtde = Integer.toString(c.getValue().getQuantidade());
+            return new ReadOnlyStringWrapper(strQtde);
+        });
+
+        TableColumn<Item, String> col4 = new TableColumn<>("Valor Unitario");
+        col4.setCellValueFactory(c -> {
+            String strValor = Double.toString(c.getValue().getProduto().getValorUnitario());
+            return new ReadOnlyStringWrapper(strValor);
+        });
+        TableColumn<Item, Void> col5 = new TableColumn<>("Opcoes");
+        Callback<TableColumn<Item, Void>, TableCell<Item, Void>> cellFactory
+                = (tablecolumn) -> new TableCell<>() {
+                    private Button btnApagar = new Button("Apagar");
+                    {
+                        btnApagar.setOnAction( e -> {
+                            Item i = controle.listaProperty().get( getIndex() );
+                            controle.remover(i);
+                        });
+                    }
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic( btnApagar );
+                        }
+                    }
+                };
+        col5.setCellFactory( cellFactory );
+        tabela.getColumns().addAll(col1, col2, col3, col4, col5);
+        tabela.setItems(controle.listaProperty());
+
+        tabela.getSelectionModel().selectedItemProperty().addListener(
+            (obs, antigo, novo) -> { 
+                System.out.println("Contato selecionado ==> " + novo);
+                controle.itensParaTela(novo);
+            }
+        );
+    }
+
     //metodo pra deixar alguns textfields receberem apenas numeros
-    public void aplicarFiltroNumerico(TextField campo){
+    private void aplicarFiltroNumerico(TextField campo){
         campo.setTextFormatter(new TextFormatter<>(change -> {
             String novoTexto = change.getControlNewText();
             return novoTexto.matches("\\d*") ? change : null;
         }));
     
+    }
+    private void mensagem(String msg){
+        new Alert(AlertType.INFORMATION, msg, ButtonType.OK).show();
     }
 
     public void start(Stage stage) throws SQLException { 
@@ -59,6 +122,7 @@ public class AtendenteFronteira extends Application{
         aplicarFiltroNumerico(txtCodigoComanda);
 
         bindings();
+        tableCreation();
 
         RowConstraints titulo = new RowConstraints();
         titulo.setPrefHeight(40);
@@ -69,50 +133,77 @@ public class AtendenteFronteira extends Application{
         c1.setPercentWidth(20);
         paneForm.getColumnConstraints().addAll(c1, c1);
 
-        Scene scn = new Scene(panePrincipal, 800, 600);
+        Scene scn = new Scene(panePrincipal, 600, 600);
 
-        txtCodigoComanda.setMaxWidth(100);
-        txtCodigoProduto.setMaxWidth(100);
-        txtQuantidade.setMaxWidth(100);
-        txtCodigoFormaPagamento.setMaxWidth(100);
+        txtCodigoComanda.setMaxWidth(90);
+        txtCodigoProduto.setMaxWidth(90);
+        txtQuantidade.setMaxWidth(50);
         
-        Label comandaAtual = new Label("Digite o id da comanda atual:");
+
+        
+        Label comandaAtual = new Label("Comanda atual:");
         comandaAtual.setStyle("-fx-font-weight: bold;");
         paneForm.add(comandaAtual, 0, 0);
-        paneForm.add(new Label("Codigo da comanda:"), 0, 1);
+        paneForm.add(new Label("Codigo Comanda"), 0, 1);
         paneForm.add( txtCodigoComanda, 1, 1);
+        Button btnComanda = new Button("Carregar");
+        paneForm.add( btnComanda, 0, 2);
 
-        Label addItens = new Label("Adicionar produto");
+        btnComanda.setOnAction( evento -> {
+            try {
+                if (controle.buscarComanda(Long.parseLong(txtCodigoComanda.getText()))){
+                    controle.atualizaTabela();
+                    mensagem("Comanda selecionada");
+                } else {
+                    mensagem("Código de comanda inválido");
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+            
+        });
+
+        Label addItens = new Label("Adicionar item");
         addItens.setStyle("-fx-font-weight: bold;");
         paneForm.add(addItens, 0, 3);
-        paneForm.add( new Label("Codigo do produto:"), 0, 4);
+        paneForm.add( new Label("Codigo Produto"), 0, 4);
         paneForm.add( txtCodigoProduto, 1, 4);
         paneForm.add( new Label("Quantidade:"), 0, 5);
         paneForm.add( txtQuantidade, 1, 5);
 
-        Label addFormaPagamento = new Label("Digite o id da forma de pagamento:");
-        addFormaPagamento.setStyle("-fx-font-weight: bold;");
-        paneForm.add(addFormaPagamento, 0, 6);
-        paneForm.add( new Label("Codigo da forma de pagamento"), 0, 7);
-        paneForm.add( txtCodigoFormaPagamento, 1, 7);
+ 
 
         Button btnSalvar = new Button("Salvar");
 
         btnSalvar.setOnAction( evento -> {
-
-            new Alert(AlertType.INFORMATION, 
-                "Pedido finalizado com sucesso!", 
-                            ButtonType.OK).show();
+            if(controle.validarComanda(Long.parseLong(txtCodigoComanda.getText()))){
+                if (controle.addItem()){
+                    mensagem("Item gravado com sucesso");
+                } else {
+                    mensagem("Produto não encontrado");
+                }
+            } else {
+                mensagem("Comanda inválida para gravar item");
+            }
+            
+            
         });
 
-        paneBotoes.getChildren().addAll(btnSalvar);
+        Button btnPesquisar = new Button("Pesquisar");
 
-        paneForm.add( paneBotoes, 0, 9, 2, 1);
+        btnPesquisar.setOnAction( evento -> {
+            //metodo do controle para pesquisar um item da comanda, talvez nao seja usado
+        });
+
+        paneBotoes.getChildren().addAll(btnSalvar, btnPesquisar);
+
+        paneForm.add( paneBotoes, 0, 7, 2, 1);
         
         panePrincipal.setTop( paneForm );
+        panePrincipal.setCenter( tabela );
 
         stage.setScene(scn);
-        stage.setTitle("Tela de pagamento");
+        stage.setTitle("Comanda");
         stage.show();
     }
 	public static void main(String[] args) {
