cb04c76b006f84023df0ad599708a1ce684c7230
JM <oshirohiro00@gmail.com>
2025-06-23 22:09:26 -0300

    Update Pedido.java

--- programa/app/src/main/java/org/model/Pedido.java
+++ programa/app/src/main/java/org/model/Pedido.java
@@ -3,43 +3,43 @@ package org.model;
 import java.time.LocalDate;
 
 public class Pedido {
-	
-	private long codigo;
-	private LocalDate dataHora = LocalDate.now();
+    private int codigo;
+	private LocalDate dataHora = LocalDate.now();;
+	private double valorTotal;
 	private Comanda comanda = new Comanda();
 	private FormaPagamento formaPagamento = new FormaPagamento();
-	
-	public long getCodigo() {
-		return codigo;
-	}
-	
-	public void setCodigo(long codigo) {
-		this.codigo = codigo;
-	}
+    
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public LocalDate getDataHora() {
+        return dataHora;
+    }
+    public void setDataHora(LocalDate dataHora) {
+        this.dataHora = dataHora;
+    }
+    public double getValorTotal() {
+        return valorTotal;
+    }
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    public Comanda getComanda() {
+        return comanda;
+    }
+    public void setComanda(Comanda comanda) {
+        this.comanda = comanda;
+    }
+    public FormaPagamento getFormaPagamento() {
+        return formaPagamento;
+    }
+    public void setFormaPagamento(FormaPagamento formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
 
-	public LocalDate getDataHora() {
-		return dataHora;
-	}
+    
 
-	public void setDataHora(LocalDate dataHora) {
-		this.dataHora = dataHora;
-	}
-	
-	public Comanda getComanda() {
-		return comanda;
-	}
-	
-	public void setComanda(Comanda comanda) {
-		this.comanda = comanda;
-	}
-	
-	public FormaPagamento getFormaPagamento() {
-		return formaPagamento;
-	}
-	
-	public void setFormaPagamento(FormaPagamento formaPagamento) {
-		this.formaPagamento = formaPagamento;
-	}
-	
-	
 }
