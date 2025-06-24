0125e45d540aeb86bacdce1747a4e76090028d24
JM <oshirohiro00@gmail.com>
2025-06-24 03:06:07 -0300

    Update ComandaControle.java

--- programa/app/src/main/java/org/control/ComandaControle.java
+++ programa/app/src/main/java/org/control/ComandaControle.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 import org.model.*;
 
+import javafx.beans.property.FloatProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleFloatProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -23,13 +25,14 @@ public class ComandaControle {
     private PadariaDAO padariaDAO = new PadariaDAOImplementation();
     private LongProperty codigoProduto = new SimpleLongProperty(0);
     private StringProperty nomeProduto = new SimpleStringProperty("");
-    private IntegerProperty quantidade = new SimpleIntegerProperty(0);
+    private IntegerProperty quantidade = new SimpleIntegerProperty(00);
+    private FloatProperty valorTotal = new SimpleFloatProperty(0f);
     private Comanda comandaAtual = null;
 
     public boolean buscarComanda(long codigo){
-        Comanda teste = padariaDAO.getComanda(codigo);
-        if (teste != null){
-            this.comandaAtual = teste;
+        Comanda c = padariaDAO.getComanda(codigo);
+        if (c != null){
+            this.comandaAtual = c;
             return true;
         }
         
@@ -47,14 +50,14 @@ public class ComandaControle {
 
     public boolean addItem(){
         Produto produto = verificaCodigoProduto();
-        if (produto != null){ //se produto existe
-            if (padariaDAO.verificaProdutoLista(comandaAtual.getCodigo(), codigoProduto.get())){ //se produto já existe na lista, editar quantidade
+        if (produto != null){
+            if (padariaDAO.verificaProdutoLista(comandaAtual.getCodigo(), codigoProduto.get())){
                 padariaDAO.editarQuantidadeProduto(quantidade.get(), comandaAtual.getCodigo(), codigoProduto.get());
             } else {
                 Item item = telaParaItens(produto);
                 padariaDAO.guardar(item, comandaAtual.getCodigo());
             }
-            atualizaTabela();
+            atualizarTabela();
             return true;
         } 
         return false; 
@@ -68,14 +71,28 @@ public class ComandaControle {
 
     public void remover(Item item){
         padariaDAO.excluir(comandaAtual.getCodigo(), item.getProduto().getCodigo());
-        atualizaTabela();
+        atualizarTabela();
     }
 
-    public void atualizaTabela(){
+    public void atualizarTabela(){
         lista.clear();
         lista.addAll(padariaDAO.getItens(comandaAtual.getCodigo()));
     }
 
+    public void atualizarValorTotal(){
+        if(validarComanda(comandaAtual.getCodigo())) {
+            Float total = padariaDAO.calculaValorTotal(comandaAtual.getCodigo());
+            if (total != null){
+                valorTotal.set(total);
+            }
+        }  
+    }
+
+    public void finalizarComanda(){
+        padariaDAO.finalizarPedido(comandaAtual);
+        limparComandaAtual();
+        atualizarTabela();
+    }
 
     private Item telaParaItens(Produto produto){
         Item item = new Item();
@@ -91,7 +108,15 @@ public class ComandaControle {
 
     }
 
-
+    private void limparComandaAtual() {
+        if(comandaAtual != null){
+            lista.clear();
+            valorTotal.set(0);
+            codigoProduto.set(0);
+            quantidade.set(0);
+            comandaAtual.getItens().clear();
+        }
+    }
 
     public LongProperty codigoProdutoProperty(){
         return this.codigoProduto;
@@ -103,6 +128,10 @@ public class ComandaControle {
 
     public IntegerProperty quantidadeProperty(){
         return this.quantidade;
+    }
+
+    public FloatProperty valorTotalProperty(){
+        return this.valorTotal;
     }
 
     public ObservableList<Item> listaProperty(){
