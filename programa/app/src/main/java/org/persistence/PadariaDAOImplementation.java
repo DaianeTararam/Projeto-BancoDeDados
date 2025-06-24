deb5122a796133f5f2fee28e9f3dca7bb0698f2a
JM <oshirohiro00@gmail.com>
2025-06-23 22:11:09 -0300

    Update PadariaDAOImplementation.java

--- programa/app/src/main/java/org/persistence/PadariaDAOImplementation.java
+++ programa/app/src/main/java/org/persistence/PadariaDAOImplementation.java
@@ -12,130 +12,225 @@ import org.model.Pedido;
 
 public class PadariaDAOImplementation implements PadariaDAO{
 	private Connection c = null;
-    private String hostName = "localhost";
+        private String hostName = "localhost";
 	private String userName = "sa";
 	private String password = "123456";
         private String dbName = "padariaBD";
 	
-	//Carrega a database
+	//carrega a database
 	public PadariaDAOImplementation() {
 		try {
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
 		    c = DriverManager.getConnection(
-			String.format("jdbc:jtds:sqlserver://%s:1433;databaseName=%s;user=%s;password=%s",
+				String.format("jdbc:jtds:sqlserver://%s:1433;databaseName=%s;user=%s;password=%s",
 						hostName, dbName, userName, password)
-		    );
-	        } catch (ClassNotFoundException e) {
-	            e.printStackTrace();
-	        } catch(SQLException e) { 
-	            e.printStackTrace();
-	        }
-	}
-
-	//--MÉTODOS QUE GUARDAM PARA SEREM IMPLEMENTADOS...
-
-	//--Guarda a quantidade de produtos (REFERENTE A TABELA ProdutoPedido)
-	public void novoProdutoPedido(ProdutoPedido produtoPedido) {
-		String sql = "INSERT INTO ProdutoPedido" +
-		             "VALUES (?, ?, ?)";
-		System.out.println("novoProdutoPedido() SQL: " + sql);
-		try{
-			PreparedStatement stm = c.prepareStatement(sql);
-			stm.setInt(1, produtoPedido.getQuantidade() );
-			stm.setObject(2, produtoPedido.getProduto() );
-			stm.setObject(3, produtoPedido.getPedido() );
-		} catch (SQLException e) { 
+				);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch(SQLException e) { 
             e.printStackTrace();
         }
 	}
 
 
-	//--Guarda um pedido (REFERENTE A TABELA Pedido)
-	public void novoPedido(Pedido pedido) {
-		String sql = "INSERT INTO Pedido (comandaCodigo, formaPagamentoCodigo)" +
-		             "VALUES (?, ?)";
-		System.out.println("novoPedido() SQL: " + sql);
-		try{
+	//verifica se o codigo da comanda digitado existe e gera Comanda (gerarComanda(rs))
+	public Comanda getComanda(long codigo){
+		String sql = "SELECT * FROM Comanda WHERE codigo = ?";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setLong(1, codigo);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()){
+				return gerarComanda(rs);
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private Comanda gerarComanda(ResultSet rs){
+		Comanda comanda = new Comanda();
+		try {
+			comanda.setCodigo(rs.getLong("codigo"));
+			comanda.setItens(getItens(comanda.getCodigo()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return comanda;
+	}
+	
+	//seleciona os itens vincuados ao codigo da comanda e cria uma lista de itens (o gerarItem(rs) cria Item para adicionar na lista)
+	public List<Item> getItens(long comanda){
+		List<Item> listaItens = new ArrayList<>();
+		String sql = "SELECT produtoCodigo, quantidade FROM ItemComanda WHERE comandaCodigo = ?";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setLong(1, comanda);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				listaItens.add(gerarItem(rs));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listaItens;
+	}
+
+	private Item gerarItem(ResultSet rs){
+		Item item = new Item();
+			try {
+				item.setProduto(getProduto(rs.getLong("produtoCodigo")));
+				item.setQuantidade(rs.getInt("quantidade"));
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		return item;
+	}
+
+	//insere novo item no bd
+	public void guardar(Item item, long codigoComanda){
+		String sql = "INSERT INTO ItemComanda (comandaCodigo, produtoCodigo, quantidade) VALUES (?, ?, ?)";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setLong(1, codigoComanda);
+			ps.setLong(2, item.getProduto().getCodigo());
+			ps.setInt(3, item.getQuantidade());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean verificaProdutoLista(long codigoComanda, long codigoProduto){
+		String sql = "SELECT i.comandaCodigo FROM ItemComanda i LEFT OUTER JOIN Produto p ON i.produtoCodigo = p.codigo " + 
+						"WHERE i.produtoCodigo = ? AND i.comandaCodigo = ?";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setLong(1, codigoProduto);
+			ps.setLong(2, codigoComanda);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()){
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public void editarQuantidadeProduto(int quantidade, long codigoComanda, long codigoProduto){
+		String sql = "UPDATE ItemComanda SET quantidade = ? WHERE produtoCodigo = ? AND comandaCodigo = ?";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setInt(1, quantidade);
+			ps.setLong(2, codigoProduto);
+			ps.setLong(3, codigoComanda);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	//precisa add metodo p/ atualizar o valor
+	public void excluir(long codigoComanda, long codigoProduto){
+		String sql = "DELETE FROM ItemComanda WHERE comandaCodigo = ? AND produtoCodigo = ?";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setLong(1, codigoComanda);
+			ps.setLong(2, codigoProduto);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	//talvez add no table view um valot total de cada produto, fazer um metodo pra calcular (usar joins)
+
+	//pega as informacoes do produto pelo codigo
+	public Produto getProduto(long codigo){
+		String sql = "SELECT * FROM Produto WHERE codigo = ?";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setLong(1, codigo);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()){
+				return gerarProduto(rs); //metodo separado p/ gerar Produto
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null; //se o codigo nao existe, retorna null
+	}
+
+	private Produto gerarProduto(ResultSet rs){
+		Produto produto = new Produto();;
+		try {
+			produto.setCodigo(rs.getInt("codigo"));
+			produto.setNome(rs.getString("nome"));
+			Categoria categoria = new Categoria();
+			categoria = getCategoria(rs.getLong("categoriaCodigo"));
+			produto.setCategoria(categoria);
+			produto.setValorUnitario(rs.getDouble("valorUnitario"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return produto;
+	}
+	//metodo auxiliar p/ criar Categoria p/ ser adicionado em Produto
+	private Categoria getCategoria(long codigo){
+		Categoria categoria = new Categoria();
+		String sql = "SELECT nome FROM Categoria WHERE codigo = ?";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setLong(1, codigo);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()){
+				categoria.setCodigo(codigo);
+				categoria.setNome(rs.getString("nome"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return categoria;
+	}
+	
+	
+	//esse metodo foi adicionado antes, precisa rever a utilidade dele, provavel que seja
+	//para mostrar no TableView, pq precisa pegar alguns valores especificos (codigo, nome e valor unitario do produto e a quantidade pedida) 
+	public List<Item> lerTodosItens(int codigo) {
+		List<Item> lista = new ArrayList<>();
+		String sql = "SELECT  p.codigo, p.nome, p.valorUnitario FROM Comanda c, Produto p, Item i WHERE c.codigo = i.comandaCodigo AND p.codigo = i.produtoCodigo AND c.codigo = ?";
+        PreparedStatement ps;
+		try {
+			ps = c.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			//
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+        //continuar...
+		return lista;
+	}
+	//mesma coisa, foi adicionado antes das modificacoes, provavelmente vai ser substituido
+	//a ideia é verificar se a comanda existe para poder mostrar os itens dela na tela
+	public Comanda verificaComanda(int codigo){
+        Comanda comanda = new Comanda();
+		String sql = "SELECT codigo FROM comanda WHERE ?";
+		try {
 			PreparedStatement stm = c.prepareStatement(sql);
-			stm.setObject(1, pedido.getComanda() );
-			stm.setObject(2, pedido.getFormaPagamento() );
-		} catch (SQLException e) { 
-            e.printStackTrace();
-        }
+			stm.setInt(1, codigo);
+			ResultSet rs = stm.executeQuery();
+			if (rs.next()){
+				return comanda;//metodo que lê os itens da comanda para a tabela
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
-
-
-	//--CONSULTAS PARA SEREM IMPLEMENTADAS....
-	
-	
-	//--Valor total de uma comanda específica
-	public void consultaValorTotalComanda(Comanda comanda) {
-		String sql = "SELECT c.codigo, SUM(pp.quantidade * pr.valorUnitario) AS totalComanda " +
-		             "FROM Produto pr INNER JOIN ProdutoPedido pp" +
-					 "ON pr.codigo = pp.produtoCodigo" +
-					 "INNER JOIN Pedido pe" +
-					 "ON pe.codigo = pp.pedidoCodigo" +
-					 "INNER JOIN Comanda c" +
-					 "ON c.codigo = pe.comandaCodigo" +
-					 "WHERE c.codigo = ?" +              //--Aqui deve colocar o id da comanda que se deseja saber o valor total
-					 "GROUP BY c.codigo";
-		System.out.println("consultaValorTotalComanda() SQL: " + sql);
-		try{
-			PreparedStatement stm = c.prepareStatement(sql);
-			stm.setObject(1, comanda.getCodigo() );
-		} catch (SQLException e) { 
-            e.printStackTrace();
-        }
-	}
-
-	//--Consulta de todos os pedidos anteriores
-	public void consultaTodosPedidos() {
-		String sql = "SELECT pe.codigo AS codigoPedido, c.codigo AS codigoComanda," +
-					 "SUM(pr.valorUnitario * pp.quantidade) AS valorTotal, fp.nome AS formaPagamento, pe.dataHora" +
-					 "FROM Produto pr INNER JOIN ProdutoPedido pp" +
-					 "ON pr.codigo = pp.produtoCodigo" +
-					 "INNER JOIN Pedido pe" +
-					 "ON pe.codigo = pp.pedidoCodigo" +
-					 "INNER JOIN Comanda c" +
-					 "ON c.codigo = pe.comandaCodigo" +
-					 "INNER JOIN FormaPagamento fp" +
-					 "ON fp.codigo = pe.formaPagamentoCodigo" +
-					 "GROUP BY pe.codigo, c.codigo, fp.nome, pe.dataHora";
-		System.out.println("consultaTodosPedidos() SQL: " + sql);
-	}
-
-	//--Total de vendas (R$) realizadas por todos os pedidos
-	public void consultaTotalVendasPedidos() {
-		String sql = "SELECT SUM(pr.valorUnitario * pp.quantidade) AS valorTotalVendas" +
-					 "FROM Produto pr INNER JOIN ProdutoPedido pp" +
-					 "ON pr.codigo = pp.produtoCodigo" +
-					 "INNER JOIN Pedido pe" +
-					 "ON pe.codigo = pp.pedidoCodigo";
-		System.out.println("consultaTotalVendasPedidos() SQL: " + sql);
-	}
-
-	//--Quantidade total de produtos vendidos por categoria
-	public void consultaQtdTotalProdutosVendidosCategoria() {
-		String sql = "SELECT SUM(pp.quantidade) AS qtdProdutosVendidos" +
-					 "FROM Categoria ct INNER JOIN Produto pr" +
-					 "ON ct.codigo = pr.CategoriaCodigo" +
-					 "INNER JOIN ProdutoPedido pp" +
-					 "ON pr.codigo = pp.produtoCodigo" +
-					 "GROUP BY ct.nome";
-		System.out.println("consultaQtdTotalProdutosVendidosCategoria() SQL: " + sql);
-	}
-	
-	//--Pedidos com valor total acima de R$20
-	public void consultaPedidosValorTotalAcima() {
-		String sql = "SELECT pe.codigo, SUM(pp.quantidade * pr.valorUnitario) AS totalPedido" +
-					 "FROM Produto pr INNER JOIN ProdutoPedido pp" +
-					 "ON pr.codigo = pp.produtoCodigo" +
-					 "INNER JOIN Pedido pe" +
-					 "ON pe.codigo = pp.pedidoCodigo" +
-					 "GROUP BY pe.codigo" +
-					 "HAVING SUM(pp.quantidade * pr.valorUnitario) > 20";
-		System.out.println("consultaPedidosValorTotalAcima() SQL: " + sql);
-	}
-	
-
 }
